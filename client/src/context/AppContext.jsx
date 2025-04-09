7f4575a2e0d36f2fa6e8225330cd7bf3da0cb79b
BLACKEYE-ASTRO <akashshingare2003@gmail.com>
2025-04-09 17:18:33 +0530

    Components and pages updated

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -15,6 +15,8 @@ export const AppContextProvider = ({children})=>{
     const [showUserLogin , setShowUserLogin] = useState(false)
     const [products , setProducts] = useState([])
     const [cartItems,setCartItems] =useState({})
+    const [searchQuery,setSearchQuery] =useState({})
+
 
     const fetchProducts = async ()=>{
         setProducts(dummyProducts)
@@ -60,7 +62,7 @@ export const AppContextProvider = ({children})=>{
     useEffect(()=>{
         fetchProducts()
     },[])
-    const value = {navigate,user,setUser,isSeller,setSeller,showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeFromCart,cartItems}
+    const value = {navigate,user,setUser,isSeller,setSeller,showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeFromCart,cartItems,searchQuery,setSearchQuery}
     
     return <AppContext.Provider value={value}>
         {children}
