ebfeb632a82acdd89c68399fcbd15af9c27b5c83
BLACKEYE-ASTRO <akashshingare2003@gmail.com>
2025-06-05 02:32:35 +0530

    Server added

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -9,6 +9,6 @@ try {
         res.json({success:true,message:"Cart Updated"});
     } catch (error) {
         console.log(error.message);
-        res.json({success:false,message:error.message})
+        res.json({success:false,message:error.message});
     }
 }
