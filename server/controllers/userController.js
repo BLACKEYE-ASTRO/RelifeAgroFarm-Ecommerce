7e299d55da5603f57575c515394d15a8dcee2b3e
BLACKEYE-ASTRO <akashshingare2003@gmail.com>
2025-06-09 19:27:30 +0530

    All files are updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -51,7 +51,7 @@ export const login = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+            secure: process.env.NODE_ENV === 'production' ,
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000,
 
